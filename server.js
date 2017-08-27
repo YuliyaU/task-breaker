76666f90258ff600c1c51d019a814303e66789b3
YulijaU <cenite-moment@mail.ru>
2017-08-27 10:46:48 +0700

    [server.js] Creates 'documents' collection and adds this call to MongoClient.connect

--- server.js
+++ server.js
@@ -6,10 +6,24 @@ const app = express();
 const port = 3000;
 const url = 'mongodb://localhost:27017/tasksdb';
 
+var insertDocuments = function(db, callback) {
+    // Get the documents collection
+    var collection = db.collection('documents');
+    // Insert some documents
+    collection.insertMany([
+      {a : 1}, {a : 2}, {a : 3}
+    ], function(err, result) {      
+      console.log("Inserted 3 documents into the collection");
+      callback(result);
+    });
+  }
+
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     console.log('Database is created!');
-    db.close();
+    insertDocuments(db, function() {
+        db.close();
+    });
 });
 
 app.listen(port, () => {
