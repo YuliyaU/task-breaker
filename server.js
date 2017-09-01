24d27783a1836a209112ab9219cfca1ffd2c3b1d
YulijaU <cenite-moment@mail.ru>
2017-09-01 14:06:18 +0700

    [server.js] Comments MongoDB part

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 
 const port = 3000;
-const url = 'mongodb://localhost:27017/tasksdb';
+// const url = 'mongodb://localhost:27017/tasksdb';
 
-var insertDocuments = function(db, callback) {
-    // Get the documents collection
-    var collection = db.collection('documents');
-    // Insert some documents
-    collection.insertMany([
-      {a : 1}, {a : 2}, {a : 3}
-    ], function(err, result) {      
-      console.log("Inserted 3 documents into the collection");
-      callback(result);
-    });
-  };
+// var insertDocuments = function(db, callback) {
+//     // Get the documents collection
+//     var collection = db.collection('documents');
+//     // Insert some documents
+//     collection.insertMany([
+//       {a : 1}, {a : 2}, {a : 3}
+//     ], function(err, result) {      
+//       console.log("Inserted 3 documents into the collection");
+//       callback(result);
+//     });
+//   };
 
-  var findDocuments = function(db, callback) {
-    // Get the documents collection
-    var collection = db.collection('documents');
-    // Find some documents
-    collection.find({'a': 3}).toArray(function(err, docs) {      
-      console.log("Found the following records");
-      console.log(docs)
-      callback(docs);
-    });
-  };
+//   var findDocuments = function(db, callback) {
+//     // Get the documents collection
+//     var collection = db.collection('documents');
+//     // Find some documents
+//     collection.find({'a': 3}).toArray(function(err, docs) {      
+//       console.log("Found the following records");
+//       console.log(docs)
+//       callback(docs);
+//     });
+//   };
 
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    console.log('Database is created!');
-    insertDocuments(db, function() {
-        findDocuments(db, function() {
-            db.close();
-        });
-    });
-});
+// MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//     console.log('Database is created!');
+//     insertDocuments(db, function() {
+//         findDocuments(db, function() {
+//             db.close();
+//         });
+//     });
+// });
 
 app.listen(port, () => {
     console.log('We are live on ' + port);
