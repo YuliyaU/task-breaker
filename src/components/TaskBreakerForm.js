b785f5345ed2efc4bb0c6d2bd8e9cd9cc66e36ae
YulijaU <cenite-moment@mail.ru>
2017-09-13 15:08:44 +0700

    [TaskBreakerForm.js] Fixes wrong key:value assignment bag in the form

--- src/components/TaskBreakerForm.js
+++ src/components/TaskBreakerForm.js
@@ -4,8 +4,8 @@ export const TaskBreakerForm = ({onNewTask}) => {
         event.preventDefault();
         onNewTask({            
             id: 2,
-            taskName: _featureName.value,
-            featureName: _taskName.value            
+            taskName: _taskName.value,
+            featureName: _featureName.value            
         });
         // To clear input fields from previous value inputs
         _taskName.value = '';
@@ -17,7 +17,7 @@ export const TaskBreakerForm = ({onNewTask}) => {
             <label>Feature:</label>
             <input type="text"                                            
                    ref={input => _featureName = input} />
-            <label htmlFor="add_task_name">Task:</label>
+            <label>Task:</label>
             <input type="text"                                              
                    ref={input => _taskName = input} />  
             <input type="submit" value="Add a task" />
